6e01c81abc0d7161c9f2f397095f60a3dc38c989
rajeshmrsc1 <rajeshmrsc1@gmail.com>
2026-02-12 18:49:22 +0530

    CheckEvenOdd logic

--- src/core/java/programs/CheckEvenOdd.java
+++ src/core/java/programs/CheckEvenOdd.java
@@ -1,10 +1,28 @@
 package core.java.programs;
 
+import java.util.*;
+
 public class CheckEvenOdd {
+
+	static boolean isEvenOdd(int num) {
+
+		if (num % 2 == 0) {
+			System.out.println("number is even ");
+
+		} else {
+			System.out.println("number is odd  ");
+		}
+
+		return true;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter any number");
+		int num = in.nextInt();
+		isEvenOdd(num);
+in.close();
 	}
 
 }
